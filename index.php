5e0e1c146fe7585cbffb4d24cd42789831a4c794
andpost200 <andrey.chepulis@kreo.net>
2021-12-01 21:47:33 +0300

    datapicker work

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The main template file
+ * 
+ * @package WordPress
+ * @subpackage BVH
+ * @since BVH 1.0
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		if ( have_posts() ) {
+
+			// Load posts loop.
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/content/content' );
+			}
+
+			// Previous/next page navigation.
+			twentynineteen_the_posts_navigation();
+
+		} else {
+
+			// If no content, include the "No posts found" template.
+			get_template_part( 'template-parts/content/content', 'none' );
+
+		}
+		?>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php
+get_footer();
