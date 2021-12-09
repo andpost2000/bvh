6664a392c6ec23fe0967449149e1ea6d21f1437f
andpost200 <andrey.chepulis@kreo.net>
2021-12-09 22:33:01 +0300

    footer mobile style

--- footer.php
+++ footer.php
@@ -13,29 +13,12 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<?php get_template_part( 'template-parts/footer/footer', 'widgets' ); ?>
-		<div class="site-info">Fz
-			<?php $blog_info = get_bloginfo( 'name' ); ?>
-			<?php if ( ! empty( $blog_info ) ) : ?>
-				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
-			<?php endif; ?>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentynineteen' ) ); ?>" class="imprint">
-				<?php
-				/* translators: %s: WordPress. */
-				printf( __( 'Proudly powered by %s.', 'twentynineteen' ), 'WordPress' );
-				?>
-			</a>
-			<?php
-			if ( function_exists( 'the_privacy_policy_link' ) ) {
-				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-			}
-			?>
-			<?php if ( has_nav_menu( 'footer' ) ) : ?>
-				<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'twentynineteen' ); ?>">
-					<?php
+<footer id="colophon" class="page-footer">
+  <?php if ( has_nav_menu( 'footer' ) ) : ?>
+  <nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'twentynineteen' ); ?>">
+    <?php
 					wp_nav_menu(
 						array(
 							'theme_location' => 'footer',
@@ -44,10 +27,42 @@
 						)
 					);
 					?>
-				</nav><!-- .footer-navigation -->
-			<?php endif; ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+  </nav><!-- .footer-navigation -->
+  <?php endif; ?>
+  <div class="page-footer__info">
+    <div class="page-footer__socials">
+      <p><?php the_field('social_media_intro','options'); ?></p>
+
+      <?php if( get_field('twitter','options') ): ?>
+      <a href="<?php the_field('twitter','options'); ?>" target="_blank"><i class="fa fa-twitter"></i></a>
+      <?php endif; ?>
+
+      <?php if( get_field('facebook','options') ): ?>
+      <a href="<?php the_field('facebook','options'); ?>" target="_blank"><i class="fa fa-facebook"></i></a>
+      <?php endif; ?>
+
+      <?php if( get_field('instagram','options') ): ?>
+      <a href="<?php the_field('instagram','options'); ?>" target="_blank"><i class="fa fa-instagram"></i></a>
+      <?php endif; ?>
+
+      <?php if( get_field('skype','options') ): ?>
+      <a href="<?php the_field('skype','options'); ?>" target="_blank"><i class="fa fa-skype"></i></a>
+      <?php endif; ?>
+
+      <?php if( get_field('vk','options') ): ?>
+      <a href="<?php the_field('vk','options'); ?>" target="_blank"><i class="fa fa-vk"></i></a>
+      <?php endif; ?>
+
+    </div>
+    <div class="page-footer__contacts">
+      <a href="tel:<?php the_field('phone','options'); ?>">T. <?php the_field('phone','options'); ?></a>
+      <a href="mailto:<?php the_field('email','options'); ?>">E. <?php the_field('email','options'); ?></a>
+    </div>
+    <div class="page-footer__awards">
+      <img src="<?php echo get_stylesheet_directory_uri();?>/images/booking-award-bella-vista-hotel.jpg">
+    </div>
+  </div>
+</footer><!-- #colophon -->
 
 </div><!-- #page -->
 
@@ -73,4 +88,5 @@ const observer = lozad('.lozad'); // lazy loads elements with default selector a
 observer.observe();
 </script>
 </body>
+
 </html>
